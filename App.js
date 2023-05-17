5b4b3b086dc0a850dcbedf6dddb840c278022791
Amanda <eunumseioqpordeemail@gmail.com>
2023-05-17 14:27:01 -0300

    sdjhSFDHUILWFEHUISFHUSFEPHPJIOSGPJIOD

--- App.js
+++ App.js
@@ -1,12 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import RootNavigation from "./src";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+  return(
+    <RootNavigation />
   );
 }
-
--- src/screens/Homescreen.jsx
+++ src/screens/Homescreen.jsx
@@ -1,7 +1,25 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { View } from "react-native";
 import { Text } from "react-native-paper";
+import { db } from "../config/firebase";
+import { useState } from "react";
 
 export default function Homescreen() {
+    const[produtos, setProdutos] = useState([]);
+
+    async function queryProdutos(nomeDoProduto = null ){
+        try{
+            const produtosRef = collection(db, "produto");
+            const queryProdutos = query(produtosRef, where("NomeDoProduto"  , "==", "abacaxi"));
+
+            const querySnapshot = await getDocs(query);
+        } catch(error) {
+            console.log(error);
+        }     
+    }
+    
+
+
     return(
         <View>
             <Text>Home</Text>
