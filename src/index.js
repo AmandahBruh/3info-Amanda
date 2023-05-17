83a4b40746fd99c3c9bb37e5331f6eeaa98ccdf8
Amanda <eunumseioqpordeemail@gmail.com>
2023-05-17 14:35:25 -0300

    wawa

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Homescreen from "./screens/Homescreen";
+import { NavigationContainer } from "@react-navigation/native";
 
 const Stack = createNativeStackNavigator();
 
@@ -7,7 +9,7 @@ export default function RootNavigation() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Home" component={HomeScreen} />
+                <Stack.Screen name="Home" component={Homescreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
