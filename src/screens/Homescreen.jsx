c81910f26e8c4bd3e0dbadb78e0a8716c2c8bf03
Amanda <eunumseioqpordeemail@gmail.com>
2023-05-17 14:56:59 -0300

    owowowowoowowowowowoowow

--- src/screens/Homescreen.jsx
+++ src/screens/Homescreen.jsx
@@ -6,12 +6,39 @@ import { useState } from "react";
 
 export default function Homescreen() {
     const[produtos, setProdutos] = useState([]);
-    const[nomeDoProduto, setNomeDoProduto] = useState(null);
+    const [nomeDoProduto, setNomeDoProduto] = useState("");
+    const [precoDoProduto, setPrecoDoProduto] = useState("");
+    const [quantidadeDoProduto, setQuantidadeDoProduto] = useState("");
 
     async function queryProdutos(nomeDoProduto = null ){
         try{
             const produtosRef = collection(db, "produto");
             const queryProdutos = query(produtosRef, where("NomeDoProduto"  , "==", "abacaxi"));
+
+
+            const querySnapshot = await getDocs(query);
+        } catch(error) {
+            console.log(error);
+        }     
+    }
+    
+    async function queryProdutos(precoDoProduto = null ){
+        try{
+            const produtosRef = collection(db, "produto");
+            const queryProdutos = query(produtosRef, where("PrecoDoProduto"  , "==", "2,00"));
+
+
+            const querySnapshot = await getDocs(query);
+        } catch(error) {
+            console.log(error);
+        }     
+    }
+    
+    async function queryProdutos(quantidadeDoProduto = null ){
+        try{
+            const produtosRef = collection(db, "produto");
+            const queryProdutos = query(produtosRef, where("QuantidadeDoProduto"  , "==", "3"));
+
 
             const querySnapshot = await getDocs(query);
         } catch(error) {
@@ -21,6 +48,8 @@ export default function Homescreen() {
     
 
 
+
+
     return(
         <View>
             <Text>Home</Text>
@@ -28,6 +57,16 @@ export default function Homescreen() {
                 label="Nome do Produto"
                 value={nomeDoProduto}
                 onChangeText={setNomeDoProduto}
+            />
+            <TextInput
+                label="Preço do Produto"
+                value={precoDoProduto}
+                onChangeText={setPrecoDoProduto}
+            />
+            <TextInput
+                label="Quantidade do Produto"
+                value={quantidadeDoProduto}
+                onChangeText={setQuantidadeDoProduto}
             />
             <FlatList
                 data={produtos}
